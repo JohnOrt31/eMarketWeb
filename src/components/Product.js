04531690e37d933e525bf0e2618323dabc94f421
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-23 12:23:13 -0500

    animacion home mas rapida

--- src/components/Product.js
+++ src/components/Product.js
@@ -65,7 +65,7 @@ const ProductWrapper = styled.div`
   .card-footer{
     background: transparent;
     border-top: transparent;
-    transition: all 1s linear;
+    transition: all 0.3s linear;
   }
   &:hover{
     .card{
@@ -83,7 +83,7 @@ const ProductWrapper = styled.div`
   }
   
   .card-img-top{
-    transition: all 1s linear;
+    transition: all 0.3s linear;
   }
 
   .img-container:hover .card-img-top{
@@ -101,7 +101,7 @@ const ProductWrapper = styled.div`
     font-size: 1.4rem;
     border-radius: 0.5rem 0 0 0;
     transform: translate(100%, 100%);
-    transition: all 1s linear;
+    transition: all 0.3s linear;
 
   }
 
